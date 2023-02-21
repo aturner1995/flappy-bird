be0272e06929037981d98660c782e1b920594780
Aaron <aaturner1995@gmail.com>
2023-02-21 10:06:20 -0400

    finished mechanics of the game

---
+++ README.md
@@ -0,0 +1,21 @@
+# Flappy Bird Javascript
+
+## Description
+
+A flappy bird knock off game for your browser.
+
+This repo contains the HTML, CSS, and Javascript to create this flappy bird game.
+
+## Remote Site
+
+The remote site is hosted on GitHub Pages
+
+##### The remote site is located here:
+https://aturner1995.github.io/flappy-bird/
+
+## Screenshot
+
+
+
+
+
--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,11 +1,14 @@
 const bird = document.querySelector('.bird');
 const gameDisplay = document.querySelector('.game-container');
 const ground = document.querySelector('.ground');
+const scoreEl = document.querySelector('.score');
 
 let birdLeft = 220;
-let birdBottom = 100;
+let birdBottom = 250;
 let gravity = 2;
 let isGameOver = false;
+let gap = 450;
+let score = 0;
 
 const startGame = () => {
     birdBottom -= gravity;
@@ -33,22 +36,29 @@ const createObstacle = () => {
     let randomHeight = Math.random() * 60;
     let obstacleBottom = randomHeight;
     const obstacle = document.createElement('div');
+    const topObstacle = document.createElement('div');
     if (!isGameOver) {
         obstacle.classList.add('obstacle');
+        topObstacle.classList.add('topObstacle')
     }
     gameDisplay.appendChild(obstacle);
+    gameDisplay.appendChild(topObstacle);
     obstacle.style.left = obstacleLeft + 'px';
     obstacle.style.bottom = obstacleBottom + 'px';
+    topObstacle.style.left = obstacleLeft + 'px';
+    topObstacle.style.bottom = obstacleBottom + gap + 'px';
 
     const moveObstacle = () => {
         obstacleLeft -= 2
         obstacle.style.left = obstacleLeft + 'px';
+        topObstacle.style.left = obstacleLeft + 'px';
         if (obstacleLeft === -60) {
             clearInterval(timerId);
             gameDisplay.removeChild(obstacle);
+            gameDisplay.removeChild(topObstacle);
         }
         if (
-            obstacleLeft > 200 && obstacleLeft < 280 && birdLeft === 220 && birdBottom < obstacleBottom +155 ||
+            obstacleLeft > 200 && obstacleLeft < 280 && birdLeft === 220 && (birdBottom < obstacleBottom + 153 || birdBottom > obstacleBottom + gap-200) ||
             birdBottom === 0) {
             gameOver();  
             clearInterval(timerId) 
@@ -61,8 +71,16 @@ const createObstacle = () => {
     };
 }
 
+const scoreTracker = () => {
+    score += 1;
+    scoreEl.textContent ='Score: ' + score;
+}
+
+let scoreValue = setInterval(scoreTracker,3000);
+
 const gameOver = () => {
     clearInterval(gameTimerId);
+    clearInterval(scoreValue);
     isGameOver = true;
     document.removeEventListener('keyup', control)
 }
